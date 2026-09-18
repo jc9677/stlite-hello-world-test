764d676e822acedd201a8e7726f53d4ff73ecf7c
jc <jc9677@users.noreply.github.com>
2026-09-18 14:01:54 -0400

    simplify app.py

--- app.py
+++ app.py
@@ -2,7 +2,4 @@ import streamlit as st
 
 st.title("Hello, World!")
 
-name = st.text_input("What's your name?")
-
-if name:
-    st.write(f"Hello, {name}!")
+st.write("This Streamlit app is running in the browser.")
